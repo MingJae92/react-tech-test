f2bbabee437d4332a1ad8c909dcf89825e366c8d
Ming Chi <wong_mingchi@yahoo.co.uk>
2024-02-12 00:34:53 +0000

    Comments added to drinks and drinks_details component

--- src/components/Drink_Detail.jsx
+++ src/components/Drink_Detail.jsx
@@ -11,33 +11,43 @@ import {
 } from "@mui/material";
 
 function DrinkDetail() {
+  // State variables to manage drink details, loading, and error
   const [drinkDetail, setDrinkDetail] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  // Get drink id from URL parameters
   const { id } = useParams();
+  // API endpoint for fetching drink details
   const drinkDetailsApi = `https://api.punkapi.com/v2/beers/${id}`;
-  console.log(id)
+
+  // Function to fetch drink details from the API
   const fetchDrinkDetails = async () => {
     try {
+      // Make API request and update state with response data
       const drinkDetailsApiResponse = await axios.get(drinkDetailsApi);
-      setDrinkDetail(drinkDetailsApiResponse.data[0]); // { name: lager }.ma
-      console.log(drinkDetailsApiResponse)
+      setDrinkDetail(drinkDetailsApiResponse.data[0]);
     } catch (err) {
+      // Handle errors by updating the error state
       console.log(err);
       setError(err);
     } finally {
+      // Set loading to false after the request is complete
       setLoading(false);
     }
   };
 
+  // Use useEffect to fetch drink details when the component mounts or when the id changes
   useEffect(() => {
     fetchDrinkDetails();
   }, [id]);
 
+  // Display loading spinner while data is being fetched
   if (loading) {
     return <CircularProgress />;
   }
 
+  // Display error message if there was an issue fetching data
   if (error) {
     return (
       <Typography variant="body1">
@@ -46,44 +56,56 @@ function DrinkDetail() {
     );
   }
 
+  // Display message if drink details are not found
   if (!drinkDetail) {
     return <Typography variant="body1">Drink not found</Typography>;
   }
 
+  // Display drink details in a Card component
   return (
     <div>
-    
-        <div>
-          <Typography variant="h4" gutterBottom>
-            {drinkDetail.name}
-          </Typography>
-          <Card>
-            <CardMedia
-              component="img"
-              alt={drinkDetail.name}
-              height="400"
-              image={drinkDetail.image_url}
-              style={{ width: "100%" }}
-            />
-            <CardContent>
-              <Typography variant="h6" component="div" gutterBottom>
-                Alcohol By Volume (ABV): {drinkDetail.abv}%
-              </Typography>
-              <Typography
-                variant="subtitle1"
-                color="textSecondary"
-                gutterBottom
-              >
-                Tagline: {drinkDetail.tagline}
-              </Typography>
-              <Typography variant="body1">{drinkDetail.description}</Typography>
-              <Typography variant="subtitle2" color="primary">
-                First Brewed: {drinkDetail.first_brewed}
-              </Typography>
-            </CardContent>
-          </Card>
-        </div>
-  
+      <div>
+        {/* Display drink name */}
+        <Typography variant="h4" gutterBottom>
+          {drinkDetail.name}
+        </Typography>
+        {/* Card component to display drink details */}
+        <Card>
+          {/* CardMedia to display drink image */}
+          <CardMedia
+            component="img"
+            alt={drinkDetail.name}
+            height={"200"} 
+            image={drinkDetail.image_url}
+            style={{ width: "100%" }}
+          />
+          {/* CardContent to display various details about the drink */}
+          <CardContent>
+            {/* Display Alcohol By Volume (ABV) */}
+            <Typography variant="h6" component="div" gutterBottom>
+              Alcohol By Volume (ABV): {drinkDetail.abv}%
+            </Typography>
+            {/* Display drink tagline */}
+            <Typography
+              variant="subtitle1"
+              color="textSecondary"
+              gutterBottom
+            >
+              Tagline: {drinkDetail.tagline}
+            </Typography>
+            {/* Display drink description */}
+            <Typography variant="body1">{drinkDetail.description}</Typography>
+            {/* Display the first brewed date */}
+            <Typography variant="subtitle2" color="primary">
+              First Brewed: {drinkDetail.first_brewed}
+            </Typography>
+            {/* Display drink volume */}
+            <Typography variant="subtitle2" color="primary">
+              Volume: {drinkDetail.volume.value} {drinkDetail.volume.unit}
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -1,6 +1,6 @@
 // Drinks.js
 import React, { useEffect, useState } from "react";
-import { Link, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import {
   CircularProgress,
@@ -11,48 +11,69 @@ import {
   CardMedia,
   Paper,
 } from "@mui/material";
-// import DrinkDetail from "./components/DrinkDetail";
 
+// Component function to display a list of drinks
 function Drinks() {
+  // State variables for managing drinks data, loading state, and error state
   const [drinks, setDrinks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+
+  // API endpoint for drinks data
   const drink_api = "https://api.punkapi.com/v2/beers";
 
+  // Function to fetch drinks data from the API
   const drinks_data_response = async () => {
     try {
+      // Fetching drinks data from the API and updating state
       const drinks_data = await axios.get(`${drink_api}?per_page=10`);
       setDrinks(drinks_data.data);
-      console.log(drinks_data.data)
+      console.log(drinks_data.data);
     } catch (err) {
-      console.log(err);
+      // Handling error in case of API request failure
+      console.error(err);
       setError(true);
     } finally {
+      // Marking the loading state as false after API request completion
       setLoading(false);
     }
   };
 
+  // useEffect hook to trigger the API request on component mount
   useEffect(() => {
     drinks_data_response();
   }, []);
 
   return (
+    // Container Paper for the drinks component
     <Paper elevation={3} style={{ padding: "20px", margin: "20px" }}>
+      {/* Header Typography for the drinks component */}
       <Typography variant="h4" gutterBottom>
         Devil's Drinks Menu
       </Typography>
+
+      {/* Conditional rendering based on loading and error states */}
       {loading ? (
+        // Display a loading spinner if data is still loading
         <CircularProgress />
       ) : error ? (
+        // Display an error message if there's an issue fetching data
         <Typography variant="body1" color="error">
           Error fetching drinks data.
         </Typography>
       ) : (
+        // Display a grid of drink cards once data is loaded
         <Grid container spacing={2}>
           {drinks.map((drink) => (
+            // Individual drink card within the grid
             <Grid item key={drink.id} xs={12} sm={6} md={4}>
+              {/* Link to navigate to the drink details page */}
               <Card>
-                <Link to={`/drinkdetail/${drink.id}`} style={{ textDecoration: "none" }}>
+                <Link
+                  to={`/drinkdetail/${drink.id}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  {/* Drink image and details within the card */}
                   <CardMedia
                     component="img"
                     alt={drink.name}
@@ -65,9 +86,9 @@ function Drinks() {
                       {drink.name}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
+                      {/* Displaying a truncated version of the drink description */}
                       {drink.description.slice(0, 10)}...
                     </Typography>
-                    
                   </CardContent>
                 </Link>
               </Card>
@@ -75,11 +96,9 @@ function Drinks() {
           ))}
         </Grid>
       )}
-
-      {/* Route for DrinkDetail component */}
-      {/* <Route path="/drinkdetail/:id" component={DrinkDetail} /> */}
     </Paper>
   );
 }
 
+// Exporting the Drinks component as the default export
 export default Drinks;
